25734103f9aef46b134910b099e01d0724e59498
airmanxtw <airmanx@stust.edu.tw>
2021-08-31 20:56:18 +0800

    example4

---
+++ src/component/example4.vue
@@ -0,0 +1,24 @@
+<template>
+  <input type="button" @click="go()" value="button" />
+</template>
+<script>
+const axios = require("axios");
+export default {
+  methods: {
+    go() {
+      axios
+        .post("https://portal.stust.edu.tw/exteduapi/auth", {
+          userid: "airmanx",
+          password: "a6005600",
+        })
+        .then((res) => {
+          debugger;
+        })
+        .catch((err) => {
+          debugger;
+        });
+    },
+  },
+  created() {},
+};
+</script>
--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import Vue from 'vue'
 import example1 from "component/example1";
 import example2 from "component/example2";
 import example3 from "component/example3";
+import example4 from "component/example4";
 //import {setprototype} from "methods/prototype";
 
 //setprototype();
@@ -12,13 +13,14 @@ new Vue({
   render(){
     return (
       <div>
-        <example1></example1>
+        {/* <example1></example1>
         <example2></example2>
-        <example3></example3>
+        <example3></example3> */}
+        <example4></example4>
       </div>
     );
   },
   components: {
-    example1,example2,example3
+    example1,example2,example3,example4
   }
 }).$mount('#app');
